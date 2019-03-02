388e507481446d429e7e59b848ee3202bfd1003e
Wojtek Jasinski <wojtek@innomenta.se>
2019-03-02 14:07:11 +0100

    Link added and styled

--- github-example/src/components_bak/TopBar.js
+++ github-example/src/components_bak/TopBar.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Link from "redux-first-router-link";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+`;
+const StyledLi = styled.li`
+  float: left;
+`;
+const StyledLink = styled(Link)`
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+`;
 
 const TopBar = () => {
-  return <h1>TopBar</h1>;
+  return (
+    <StyledUl>
+      <StyledLi>
+        {" "}
+        <StyledLink to="/">Home</StyledLink>
+      </StyledLi>
+      <StyledLi>
+        {" "}
+        <StyledLink to="/about">About</StyledLink>{" "}
+      </StyledLi>
+      <StyledLi>
+        {" "}
+        <StyledLink to="/user/1">User 1</StyledLink>{" "}
+      </StyledLi>
+      <StyledLi>
+        {" "}
+        <StyledLink to="/user/2">User 2</StyledLink>{" "}
+      </StyledLi>
+    </StyledUl>
+  );
 };
 
 export default TopBar;
